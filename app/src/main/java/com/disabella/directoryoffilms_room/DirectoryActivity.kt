ef7ad1d2173fc8411f9bee5f8b197ab1d5ea471f
izabelladudaryk <izabelladudaryk@gmail.com>
2021-08-17 13:16:03 +0300

    add setOnQueryTextListener for searchView

--- app/src/main/java/com/disabella/directoryoffilms_room/DirectoryActivity.kt
+++ app/src/main/java/com/disabella/directoryoffilms_room/DirectoryActivity.kt
@@ -14,27 +14,92 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.disabella.directoryoffilms_room.db.Film
+import com.disabella.directoryoffilms_room.db.FilmDao
 import com.disabella.directoryoffilms_room.db.defaultFilms
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.collections.ArrayList
 
 private val adapter = RecyclerAdapter(ArrayList(defaultFilms))
+
+lateinit var films: ArrayList<Film>
+var tempFilms: ArrayList<Film> = arrayListOf<Film>()
 
 class DirectoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_directory)
 
-        val searchView = findViewById<SearchView>(R.id.searchView)
-
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         recyclerView.adapter = adapter
-
         lifecycleScope.launch(Dispatchers.Main)
         {
             adapter.addItems(filmDao.selectAll())
         }
+
+        val searchView = findViewById<SearchView>(R.id.searchView)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(newText: String?): Boolean {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    films = filmDao.selectAll() as ArrayList<Film>
+                    if (newText != null) {
+                        searchFilm(newText)
+                    }
+                    withContext(Dispatchers.Main)
+                    {
+                        adapter.addItems(tempFilms)
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    films = filmDao.selectAll() as ArrayList<Film>
+                    if (newText != null) {
+                        searchFilm(newText)
+                    }
+                    withContext(Dispatchers.Main)
+                    {
+                        adapter.addItems(tempFilms)
+                    }
+                }
+                return false
+            }
+        })
+
+        searchView.setOnCloseListener(object : androidx.appcompat.widget.SearchView.OnCloseListener,
+            SearchView.OnCloseListener {
+            override fun onClose(): Boolean {
+                adapter.addItems(films)
+                return false
+            }
+        })
     }
+}
+
+private fun searchFilm(searchText: String): ArrayList<Film> {
+    if (searchText.toLowerCase(Locale.getDefault()).isNotEmpty()) {
+        tempFilms.clear()
+        films.forEach {
+            if (it.name?.toLowerCase(Locale.getDefault())!!.contains(searchText)) {
+                tempFilms.add(
+                    Film(
+                        it.name,
+                        it.releaseYear,
+                        it.producer,
+                        it.description
+                    )
+                )
+            }
+        }
+    } else {
+        tempFilms.clear()
+        tempFilms.addAll(films)
+    }
+    return tempFilms
 }
